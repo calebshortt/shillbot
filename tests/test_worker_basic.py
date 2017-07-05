6c16941c7a611adcfc4c3ed4be06bc630b4497ea
Caleb Shortt <cshortt@uvic.ca>
2017-07-04 23:43:00 -0700

    Initial commit -- working prototype

---
+++ tests/test_worker_basic.py
@@ -0,0 +1,41 @@
+
+import unittest
+import codecs
+
+from workers.basic_worker import BasicUserParseWorker
+
+
+class TestWorkerBasic(unittest.TestCase):
+
+    def test_basic_worker_connection(self):
+        """
+        Purpose: Test regular running of worker
+        Expectation: startup system, hit the reddit user and parse the data, fail to send to mothership (exception)
+
+        :precondition: Mothership server not running
+        :return:
+        """
+        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
+
+        # Can't connect to mother, so should raise ConnectionRefusedError, but should run everything else
+        self.assertRaises(ConnectionRefusedError, worker.run)
+
+    def test_worker_parsing(self):
+        """
+        Purpose: Test regular parsing mechanisms of worker
+        Expectation: Load html file, send it to worker to parse, should return list of results
+
+        :return:
+        """
+        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
+
+        with codecs.open('test_resources/sample_GET_response.html', encoding='utf-8') as f:
+            text = f.read()
+
+        results, next_page = worker.parse_text(str(text).strip().replace('\r\n', ''))
+
+        self.assertGreater(len(results), 0)     # Check that results are returned
+        self.assertEqual(len(results[0]), 3)    # Check that results are in triplets (check formatting)
+
+    def test_worker_add_links(self):
+        pass
